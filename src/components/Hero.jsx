145276a8de2fa2a919891927bbf86a25fb37e7c9
Ashik-ch <ashikch7117@gmail.com>
2025-05-29 18:42:57 +0530

    Refactor Hero component with updated background image and content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,25 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="bg-blue-100 py-20 text-center">
-      <h2 className="text-4xl font-bold text-blue-700">
-        Deep Cleaning, Done Right
-      </h2>
-      <p className="mt-4 text-gray-600 max-w-xl mx-auto">
-        We provide professional deep cleaning services for homes and businesses
-        using eco-friendly products and trained staff.
-      </p>
-      <button className="mt-6 bg-blue-700 text-white px-6 py-3 rounded hover:bg-blue-800 transition">
-        Book a Cleaning
-      </button>
+    <section
+      className="bg-cover bg-center py-20 text-center px-10"
+      style={{
+        backgroundImage: "url('https://www.pixelstalk.net/wp-content/uploads/2016/10/Cleaning-Backgrounds-Desktop.jpg')",
+      }}
+    >
+      <div className="w-48 justify-self-end  bg-opacity-70 p-8 rounded">
+        <h2 className="text-4xl font-bold text-blue-700">
+          ClearGo Cleaning Services
+        </h2>
+        <h4 className="text-2xl mt-4 text-gray-600 max-w-xl mx-auto">
+          Where Clean Matters
+        </h4>
+        <button className="mt-6 bg-blue-700 text-white px-6 py-3 rounded hover:bg-blue-800 transition">
+          Book a Cleaning
+        </button>
+      </div>
     </section>
+
   );
 };
 
